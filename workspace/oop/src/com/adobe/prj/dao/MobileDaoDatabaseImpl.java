81a56e864de7a99a6a169675ccc2897ffda54e72
Banuprakash <banuprakashc@yahoo.co.in>
2025-08-20 11:44:23 +0530

    JDBC

--- workspace/oop/src/com/adobe/prj/dao/MobileDaoDatabaseImpl.java
+++ workspace/oop/src/com/adobe/prj/dao/MobileDaoDatabaseImpl.java
@@ -5,7 +5,6 @@ import com.adobe.prj.entity.Mobile;
 public class MobileDaoDatabaseImpl implements MobileDao{
     @Override
     public void addMobile(Mobile mobile) {
-        Object
         // INSERT INTO ... SQL statements
         System.out.println(mobile.getName() + " stored in MySQL database!!");
     }
